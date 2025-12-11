c255b8de5c6ae1fe53a47a485e753441cbf1375d
MauricioJVelazquezG <20243ds142@utez.edu.mx>
2025-12-11 04:10:32 -0600

    Agregue el Registro ViewModel

--- app/src/main/java/com/example/proyectointegradordam/ui/viewmodel/AuthUiState.kt
+++ app/src/main/java/com/example/proyectointegradordam/ui/viewmodel/AuthUiState.kt
@@ -4,5 +4,6 @@ sealed interface AuthUiState {
     data object Idle : AuthUiState             // Esperando
     data object Loading : AuthUiState          // Cargando
     data object Success : AuthUiState          // Login exitoso
+    data object RegistrationSuccess : AuthUiState //Registro exitoso
     data class Error(val mensaje: String) : AuthUiState // Error (contraseña incorrecta, etc)
 }
